820c40d5840191e85a7f1783667da406032d05e4
Hani Awni <hani.awni@gmail.com>
2019-04-22 04:53:19 -0400

    update the demo data

--- seed_test_data.sql
+++ seed_test_data.sql
@@ -9,11 +9,11 @@ INSERT INTO maps (name, building_id, mapfile,
 				  transform_scaling, transform_offset, transform_rotation)
 	VALUES ('Outside', 1, 'nope.jpg', NULL, NULL, NULL);
 
-INSERT INTO rooms (map_id, name_normal, name_formal, mapnode_id)
-VALUES	(1, 'Randolph Loading Dock', NULL, 1),
-		(1, 'Randolph Main Front Door', 'Randolph Main Door', 2),
-		(1, 'Randolph Front-Center Side Door', Null, 3),
-		(1, 'Assistive Wearable Robotics Lab', 'Rm 117A', 4);
+INSERT INTO rooms (map_id, name_normal, name_formal, x, y)
+VALUES	(1, 'Randolph Loading Dock', NULL, 15, 0),
+		(1, 'Randolph Main Front Door', 'Randolph Main Door', 20, 20),
+		(1, 'Randolph Front-Center Side Door', Null, 15, 20),
+		(1, 'Assistive Wearable Robotics Lab', 'Rm 117A', 15, 10);
 
 INSERT INTO accessibility_constraints (name)
 VALUES ('level terrain only'), ('need stable terrain or handholds'), ('need automatic doors');
